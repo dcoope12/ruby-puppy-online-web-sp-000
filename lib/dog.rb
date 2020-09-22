9f31d0b69ddf5a7ed19415519fc294b8723990ee
Devin Cooper <dcoope12@asu.edu>
2020-09-22 21:33:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,26 +1,22 @@
 class Dog
 
- attr_accessor :name
+  attr_accessor :name
 
- @@all = []
+  @@all = []
 
- def initialize(name)
-@name = name
-@@all << self
- end
-
-def self.all
-  @@all.each do |dog|
-    puts dog.name
+  def initialize(name)
+    @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
   end
 
   def self.clear_all
     @@all.clear
-  end
-
-  def self.print_all
-    puts dog.name
   end
 
 end
